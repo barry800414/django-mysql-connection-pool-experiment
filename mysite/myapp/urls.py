eb11d72e75942cf26fc91ae39ba0b31eadce5dd1
Barry Chen <barry.chen@appier.com>
2022-09-25 20:32:45 +0800

    feat: test close connection

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sleep/<int:secs>', views.sleep, name='sleep'),
+    path('close/', views.close_connection, name='close_connection'),
+
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,7 +1,10 @@
 
+import time
+
 # Create your views here.
 from mysite.myapp.models import User
 from django.http import HttpResponse
+from django.db import close_old_connections
 
 def index(request):
     print('start view', flush=True)
@@ -9,4 +12,28 @@ def index(request):
     new_user = User(name='new_user', password='password')
     new_user.save()
     print('going to response', flush=True)
+    return HttpResponse(user.name)
+
+
+def sleep(request, secs):
+    print('start view', flush=True)
+    if secs > 0:
+        time.sleep(secs)
+    user = User.objects.get(id=1)
+    new_user = User(name='new_user', password='password')
+    new_user.save()
+    # close_old_connections()
+    print('after ORM model', flush=True)
+    if secs > 0:
+        time.sleep(secs)
+    print('going to response', flush=True)
+    return HttpResponse(user.name)
+
+
+def close_connection(request):
+    print('start view', flush=True)
+    user = User.objects.get(id=1)
+    new_user = User(name='new_user', password='password')
+    new_user.save()
+    close_old_connections()
     return HttpResponse(user.name)
